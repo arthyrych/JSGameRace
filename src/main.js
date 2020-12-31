4c88b60392758febcfb83d033ee905d1e631da80
Artem <arthyrych@gmail.com>
2020-12-31 11:13:30 +0200

    refactored

--- src/main.js
+++ src/main.js
@@ -1,81 +1,83 @@
-    // returns a random number between min and max (both included)
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    };
+// returns a random number between min and max (both included)
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
 
 
-    // creating constructor
-    function Car (x, y) {
-        this.x = x;
-        this.y = y;
-        this.draw();
-    };
+// creating constructor
+function Car (x, y) {
+    this.x = x;
+    this.y = y;
+    this.draw();
+};
 
 
-    // creating a draw method for our objects
-    Car.prototype.draw = function () {
+// creating a draw method for our objects
+Car.prototype.draw = function () {
 
-        let carHtml = '<img src="../race/img/car.png">';
+    // taking location of the car img
+    let carHtml = '<img src="../race/img/car.png">';
 
-        this.carElement = $(carHtml);
+    this.carElement = $(carHtml);
 
-        this.carElement.css({
-            position : 'absolute',
-            left: this.x,
-            top : this.y
-        })
+    this.carElement.css({
+        position : 'absolute',
+        left: this.x,
+        top : this.y
+    })
 
-        $('body').append(this.carElement);
-    };
-
-
-    // moving object right
-    Car.prototype.moveRight = function () {
-
-        this.x += getRndInteger(0, 5);
-        this.carElement.css({
-            left : this.x,
-            top : this.y
-        });
-    };
+    // appearing the car img on the page
+    $('body').append(this.carElement);
+};
 
 
-    let firstCar = new Car(20, 130);
-    let secondCar = new Car (20, 230);
+// moving object right
+Car.prototype.moveRight = function () {
 
-    // var to check if the race was started
-    let raceIsStarted = false;
+    // moving for a random number between inputs
+    this.x += getRndInteger(0, 5);
 
-
-    // starting a race between cars
-    function startRace () {
-
-
-        // checking if the race was started
-        if (raceIsStarted) {
-            return;
+    this.carElement.css({
+        left : this.x,
+        top : this.y
+    });
+};
 
 
-        // starting if the race was not started
-        } else {
+// creating cars and setting theirs positions x and y on the page
+let firstCar = new Car(20, 130);
+let secondCar = new Car (20, 230);
 
-            raceIsStarted = true;
 
-            setInterval(() => {
-            firstCar.moveRight();
+// var to check if the race was started
+let raceIsStarted = false;
+
+
+// starting a race between cars
+function startRace () {
+
+    // checking if the race was started
+    if (raceIsStarted) {
+        return;
+
+    // starting if the race was not started
+    } else {
+
+        raceIsStarted = true;
+
+        setInterval(() => {
+        firstCar.moveRight();
         }, 15);
 
         setInterval(() => {
-            secondCar.moveRight();
+        secondCar.moveRight();
         }, 15);
-
-        }
-
-    };
+    }
+};
     
 
-    // stopping a race between cars
-    function stopRace () {
-        location.reload();
-        return false;
-    };
+// stopping a race between cars
+function stopRace () {
+    location.reload();
+    return false;
+};
